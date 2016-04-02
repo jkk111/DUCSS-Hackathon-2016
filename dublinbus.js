eda8478044bf1db1b8e34bdce2b81ddfc3a55f72
binaryjigsaw <tpywell@gmail.com>
2016-04-02 13:23:37 +0100

    bus barebones

--- dublinbus.js
+++ dublinbus.js
@@ -1,16 +1,34 @@
 // https://github.com/jneill/dublinbus-api
 
-var BUS_STOPS = "http://dublinbus-api.heroku.com/stops";
-var BUS_SERVICES = "http://dublinbus-api.heroku.com/services";
+var BUS_STOPS_URL = "http://dublinbus-api.heroku.com/stops";
+var BUS_SERVICES_URL = "http://dublinbus-api.heroku.com/services";
 
 function getStops() {
+    var busStopsXML;
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", BUS_STOPS, true);
+    xhr.open("GET", BUS_STOPS_URL, true);
+    xhr.onload = function() {
+      console.log(parseStops(this.responseText));
+    }
     xhr.send();
 }
 
-function getServices() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", BUS_SERVICES, true);
-    xhr.send();
+function getBusServices() {
+  var busServicesXML;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", BUS_SERVICES_URL, true);
+  xhr.onload = function() {
+    console.log(parseServices(this.responseText));
+  }
+  xhr.send();
+}
+
+function parseStops(stops) {
+  //TODO
+  return parsedStops;
+}
+
+function parseBusServices(services){
+  //TODO
+  return parsedServices;
 }
