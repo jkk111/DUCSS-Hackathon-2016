9908f738708085065ce8f79681391acb93ab2408
binaryjigsaw <tpywell@gmail.com>
2016-04-02 12:48:56 +0100

    dublin bus; i think

---
+++ dublinbus.js
@@ -0,0 +1,22 @@
+var STOPS_URL = "http://dublinbus-api.heroku.com/stops";
+var SERVICES_URL = "http://dublinbus-api.heroku.com/services";
+
+function getStops() {
+    var stopsXML;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", STOPS_URL, true);
+    xhr.onload = function() {
+      console.log(parseStops(this.responseText));
+    }
+    xhr.send();
+}
+
+function getServices() {
+    var stopsXML;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", SERVICES_URL, true);
+    xhr.onload = function() {
+      console.log(parseStops(this.responseText));
+    }
+    xhr.send();
+}
