d37ec92b56ad855f43d7351b1f1b216364abce5f
Eric <moloneer@tcd.ie>
2016-04-02 15:09:19 +0100

    more traffic stuff

--- traffic_demo.js
+++ traffic_demo.js
@@ -1,3 +1,4 @@
+console.log("__");
 var lat, long_;
 var south_lat, west_long, north_lat, east_long;
 var offset = 0.02;
@@ -14,19 +15,31 @@ function showPosition(position) {
 
     lat = position.coords.latitude;
     long_ = position.coords.longitude;
+    console.log("latitude = " + lat );
+    console.log("longitude = " + long_);
 
     south_lat = lat - offset;
     north_lat = lat + offset;
     east_long = lat + offset;
     west_long = long_ - offset;
-
+    var q = buildQuery();
+    console.log(q);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", q, true);
+    xhr.onload = function() {
+        console.log(this.responseText);
+    }
+    xhr.send();
 }
 
-function buildQuery(latitude, longitude){
+function buildQuery(){
+
 	var key = "Ao5U18q_nspT-Vc6UtHWvq2XF_eCCFEVfzCmKmhXsqdB0L4lTd7HXV5RyoR8W-JJ";
 	var query = "http://dev.virtualearth.net/REST/V1/Traffic/Incidents/";
     query += [south_lat,west_long,north_lat,east_long].join(",");
-    query += "&key=" + key;
+    query += "?key=" + key;
+    return query;
 	
 }
 
+document.addEventListener("DOMContentLoaded", getLocation);
