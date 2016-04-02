d04be0fc6f5be3a47afd39ebb8c7c03ac4c3d649
John Kelly <kellyj58@tcd.ie>
2016-04-02 14:11:44 +0100

    retrieves times on stop select

--- irishrail.js
+++ irishrail.js
@@ -1,35 +1,44 @@
 var STOPS_URL = "https://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML";
 var TIMES_URL = "https://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML";
+var MAX_WAIT = 30;
+var pending;
 
-function getStops(cb) {
+function getRailStops(cb) {
   var stopsXML;
   var xhr = new XMLHttpRequest();
   xhr.open("GET", STOPS_URL, true);
   xhr.onload = function() {
-    console.log(parseStops(this.responseText));
     if(cb)
-      cb(parseStops(this.responseText));
+      cb(parseRailStops(this.responseText));
     else
       throw new Error("NO CALLBACK SPECIFIED");
   }
   xhr.send();
 }
 
-function getStationData(station, cb) {
-  var url = `${TIMES_URL}?StationDesc=${station}`;
+function getRailStationData(station, maxWait, cb) {
+  if(typeof maxWait === "function") {
+    cb = maxWait;
+    maxWait = undefined;
+  }
+  if(pending) {
+    pending.abort();
+  }
+  var url = `${TIMES_URL}?StationDesc=${station}` + (maxWait != undefined ? `&${maxWait}` : "");
   var xhr = new XMLHttpRequest();
+  pending = xhr;
   xhr.open("GET", url, true);
   xhr.onload = function() {
-    console.log(parseStationData(this.responseText));
+    pending = undefined;
     if(cb)
-      cb(parseStationData(this.responseText));
+      cb(parseRailStationData(this.responseText, station));
     else
       throw new Error("NO CALLBACK SPECIFIED");
   }
   xhr.send();
 }
 
-function parseStops(stops) {
+function parseRailStops(stops) {
   var parser = new DOMParser();
   var els = parser.parseFromString(stops, "text/xml");
   var stopNames = els.querySelectorAll("StationDesc");
@@ -40,18 +49,25 @@ function parseStops(stops) {
     stopNamesParsed.push({ name: stopNames[i].innerHTML, lon: stopLon[i].innerHTML, lat: stopLat[i].innerHTML});
   }
   console.log(stopNamesParsed.length)
+  stopNamesParsed.sort(function(a, b) {
+    if(a.name > b.name) return 1;
+    else if(a.name == b.name) return 0;
+    return -1;
+  })
   return stopNamesParsed;
 }
 
-function parseStationData(data) {
+function parseRailStationData(data, name) {
   var parser = new DOMParser();
   var els = parser.parseFromString(data, "text/xml");
   var destinations = els.querySelectorAll("Destination");
+  var departTime = els.querySelectorAll("Expdepart");
   var arrivalTime = els.querySelectorAll("Exparrival");
-  var type = els.querySelectorAll("Trainstype");
+  var type = els.querySelectorAll("Traintype");
   var trains = [];
   for(var i = 0; i < destinations.length; i++) {
-    trains.push({dest: destinations[i].innerHTML, arrivalTime: arrivalTime[i].innerHTML, type: type[i].innerHTML});
+    if(destinations[i].innerHTML !== name)
+      trains.push({dest: destinations[i].innerHTML, time: (arrivalTime[i].innerHTML == "00:00" ? departTime[i].innerHTML : arrivalTime[i].innerHTML), type: type[i].innerHTML});
   }
   return trains;
 }
--- main.js
+++ main.js
@@ -1,5 +1,33 @@
-getStops(function(data) {
-  console.log(data);
+var stops;
+document.addEventListener("DOMContentLoaded", function() {
+  getRailStops(function(data) {
+    stops = data;
+    var dropdown = document.getElementById("railStopsDropdown");
+    var railInfo = document.getElementById("rail-data");
+    for(var i = 0 ; i < data.length; i++) {
+      var el = document.createElement("option");
+      el.value = i;
+      el.innerHTML = data[i].name;
+      dropdown.appendChild(el);
+    }
+    dropdown.addEventListener("change", function() {
+      if(dropdown.value != "--------") {
+        getRailStationData(stops[dropdown.value].name, function(data) {
+          railInfo.innerHTML = "";
+          console.log(data);
+          for(var i = 0 ; i < data.length; i++) {
+            var row = document.createElement("tr");
+            var dest = document.createElement("td");
+            var time = document.createElement("td");
+            dest.innerHTML = data[i].dest;
+            time.innerHTML = data[i].time;
+            row.appendChild(dest);
+            row.appendChild(time);
+            railInfo.appendChild(row);
+          }
+        })
+      }
+    })
+  })
 })
-
 getBusStopInfo(184);
