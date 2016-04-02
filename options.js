4607dad0dd527f8783e53bf3cb4e37445fa48a34
binaryjigsaw <tpywell@gmail.com>
2016-04-02 15:03:28 +0100

    added preferables

--- options.js
+++ options.js
@@ -1,7 +1,11 @@
 function save_options() {
+  var preferedStop = document.getElementById('preferedStop').value;
+  var preferedRoute = document.getElementById('preferedRoute').value;
   var travelTime = document.getElementById('travelTime').value;
   var finishTime = document.getElementById('finishTime').value;
   chrome.storage.sync.set({
+    preferedStop: preferedStop,
+    preferedRoute: preferedRoute,
     travelTime: travelTime,
     finishTime: finishTime
   }, function() {
@@ -15,9 +19,13 @@ function save_options() {
 
 function restore_options() {
   chrome.storage.sync.get({
+    preferedStop: 0;
+    preferdRoute: 0;
     travelTime: null,
     finishTime: null
   }, function(items) {
+    document.getElementById('preferedStop').value = items.preferedStop;
+    document.getElementById('preferedRoute').value = items.preferedRoute;
     document.getElementById('travelTime').value = items.travelTime;
     document.getElementById('finishTime').checked = items.finishTime;
   });
