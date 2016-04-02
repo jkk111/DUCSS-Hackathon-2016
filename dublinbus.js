e0078f0f8936ab4c424dbbd14551a1f271339290
John Kelly <kellyj58@tcd.ie>
2016-04-02 15:22:08 +0100

    Added dublin bus output

--- dublinbus.js
+++ dublinbus.js
@@ -1,9 +1,21 @@
-var BUS_API_URL = "https://data.dublinked.ie/cgi-bin/rtpi/busstopinformation";
+var BUS_API_URL = "https://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation";
+var BUS_STOP_URL = "https://data.dublinked.ie/cgi-bin/rtpi/busstopinformation?format=json";
 
-function getBusStopInfo(stopid) {
-  var xmlhttp = new XMLHttpRequest();
+function getBusStops(cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", BUS_STOP_URL, true);
+  xhr.onload = function() {
+    cb(JSON.parse(this.responseText))
+  }
+  xhr.send();
+}
+
+function getBusStopInfo(stopid, cb) {
+  var xhr = new XMLHttpRequest();
   var url = `${BUS_API_URL}?stopid=${stopid}&format=json`;
-  xmlhttp.open("GET", url, true);
-  xmlhttp.send();
-  console.log(this.responseText);
+  xhr.open("GET", url, true);
+  xhr.onload = function() {
+    cb(JSON.parse(this.responseText));
+  }
+  xhr.send();
 }
--- main.js
+++ main.js
@@ -1,32 +1,69 @@
-var stops;
+var railStops, busStops;
+var dropdown, busStopsDropdown, railInfo, busInfo;
 document.addEventListener("DOMContentLoaded", function() {
+  dropdown = document.getElementById("railStopsDropdown");
+  busStopsDropdown = document.getElementById("busStopsDropdown");
+  dropdown.addEventListener("change", railListener);
+  busStopsDropdown.addEventListener("change", busListener);
   getRailStops(function(data) {
-    stops = data;
-    var dropdown = document.getElementById("railStopsDropdown");
-    var railInfo = document.getElementById("rail-data");
+    railStops = data;
+    railInfo = document.getElementById("rail-data");
     for(var i = 0 ; i < data.length; i++) {
       var el = document.createElement("option");
       el.value = i;
       el.innerHTML = data[i].name;
       dropdown.appendChild(el);
     }
-    dropdown.addEventListener("change", function() {
-      if(dropdown.value != "--------") {
-        getRailStationData(stops[dropdown.value].name, function(data) {
-          railInfo.innerHTML = "";
-          console.log(data);
-          for(var i = 0 ; i < data.length; i++) {
-            var row = document.createElement("tr");
-            var dest = document.createElement("td");
-            var time = document.createElement("td");
-            dest.innerHTML = data[i].dest;
-            time.innerHTML = data[i].time;
-            row.appendChild(dest);
-            row.appendChild(time);
-            railInfo.appendChild(row);
-          }
-        })
-      }
-    })
+  })
+  getBusStops(function(data) {
+    busStops = data.results;
+    busInfo = document.getElementById("bus-data");
+    console.log(data);
+    for(var i = 0 ; i < busStops.length; i++) {
+      var el = document.createElement("option");
+      el.innerHTML = busStops[i].stopid;
+      el.value = i;
+      busStopsDropdown.appendChild(el);
+    }
   })
 })
+
+function busListener() {
+  if(busStopsDropdown.value != "--------") {
+    getBusStopInfo(busStops[busStopsDropdown.value].stopid, function(data) {
+      data = data.results;
+      busInfo.innerHTML = "";
+      for(var i = 0 ; i < data.length; i++) {
+        var row = document.createElement("tr");
+        var dest = document.createElement("td");
+        var route = document.createElement("td");
+        var time = document.createElement("td");
+        dest.innerHTML = data[i].destination;
+        route.innerHTML = data[i].route;
+        time.innerHTML = data[i].departuredatetime.substring(data[i].departuredatetime.indexOf(" ") + 1).substring(0, 5);
+        row.appendChild(dest);
+        row.appendChild(route);
+        row.appendChild(time);
+        busInfo.appendChild(row);
+      }
+    })
+  }
+}
+
+function railListener() {
+  if(dropdown.value != "--------") {
+    getRailStationData(railStops[dropdown.value].name, function(data) {
+      railInfo.innerHTML = "";
+      for(var i = 0 ; i < data.length; i++) {
+        var row = document.createElement("tr");
+        var dest = document.createElement("td");
+        var time = document.createElement("td");
+        dest.innerHTML = data[i].dest;
+        time.innerHTML = data[i].time;
+        row.appendChild(dest);
+        row.appendChild(time);
+        railInfo.appendChild(row);
+      }
+    })
+  }
+}
